b98e86ae644fea97f7490ab3c29091c7770a69de
ChristosPts <cristospt@gmail.com>
2023-01-27 00:29:31 +0200

    first commit

---
+++ loader.js
@@ -0,0 +1,16 @@
+const loaderWrapper = document.querySelector(".loader-wrapper");
+if (history.scrollRestoration) {
+    history.scrollRestoration = 'manual';
+} else {
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    }
+}
+setTimeout(()=> {
+    loaderWrapper.classList.add("fade-out");
+    document.body.style.overflowY = "scroll"
+    document.body.style.pointerEvents = "all"
+},2000)
+
+
+ 
---
+++ script.js
@@ -0,0 +1,12 @@
+const card1Btn = document.querySelector(".card1");
+const card2Btn = document.querySelector(".card2");
+const card3Btn = document.querySelector(".card3"); 
+
+function  SA2card1H(){ card1Btn.classList.add('active'); }
+function  SA2card1U() { card1Btn.classList.remove('active'); }
+    
+function  SA2card2H(){ card2Btn.classList.add('active'); }
+function  SA2card2U() { card2Btn.classList.remove('active'); }
+
+function  SA2card3H(){ card3Btn.classList.add('active'); }
+function  SA2card3U() { card3Btn.classList.remove('active'); }
